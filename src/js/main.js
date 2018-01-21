3bc412b4831e8f33289144e3bec19ba6cccf4437
milena-mica <trajkovic.milena.mica@gmail.com>
2018-01-21 18:08:51 +0100

    Photo Gallery Newsletter

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,8 @@ $(function() {
     });
   });
 });
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
